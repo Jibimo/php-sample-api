8d666eda5e520dceeaeeb023e6c426efdc486002
JTAG <hesamgholami@yahoo.com>
2019-02-25 15:38:28 +0330

    * Add money request ability

--- request.php
+++ request.php
@@ -3,21 +3,22 @@
  * Created by IntelliJ IDEA.
  * User: jtag
  * Date: 2/25/19
- * Time: 2:29 PM
+ * Time: 3:31 PM
  */
 
 require_once "./functions.php";
 
 /**
- * This function will be used to pay to a user which may or may not be registered in Jibimo.
- * @param $mobileNumber string Mobile number of a person whom you want to pay to.
- * @param $amount int Amount of payment in Toomaans.
+ * This function will be used to charge a user which may or may not be registered in Jibimo.
+ * @param $mobileNumber string Mobile number of a person whom you want to charge.
+ * @param $amount int Amount of request in Toomaans.
  * @param $privacy string Privacy scope which can be one of `Public`, `Friend` or `Personal`.
  * @param $description string Transaction description which will be appear in feed.
  * @param $trackerId string A UUID which will be used as factor id.
+ * @param $returnUrl string URL to return back after payment.
  * @return bool|string
  */
-function pay($mobileNumber, $amount, $privacy, $description, $trackerId)
+function request($mobileNumber, $amount, $privacy, $description, $trackerId, $returnUrl)
 {
 
     $headers = [
@@ -33,18 +34,20 @@ function pay($mobileNumber, $amount, $privacy, $description, $trackerId)
         'privacy' => $privacy,
         'description' => $description,
         'tracker_id' => $trackerId,
+        'return_url' => $returnUrl,
     ];
 
     // Remove `sandbox` from URL if you want to call the main API in production server
-    return post("https://jibimo.com/sandbox/api/business/pay", $data, $headers);
+    return post("https://jibimo.com/sandbox/api/business/request_transaction", $data, $headers);
 }
 
-$result = pay(
+$result = request(
     '+989121271063',
     8500,
-    'Friend',
-    'پرداخت جایزه‌ی شما!',
-    '33298f08-d1e2-49fa-8585-6bd510acca38'
+    'Public',
+    'ممنون که از سرویس ما خرید کردید!',
+    '33298f08-d1e2-85fa-8585-6bd510acca38',
+    'http://localhost4/my_return_callback'
 );
 
 var_dump($result);
